f71b5a01015dab2688e36340fd9f0357756ea9fa
lmarcho <lakshitha.mathngadeera@neosolax.com>
2024-06-07 20:10:53 +0530

    connect supplier database

---
+++ backend/Hello_Shoes_Pvt._Ltd_BackEnd/src/main/java/lk/ijse/hello_shoes_shop_backend/Service/DateServices.java
@@ -0,0 +1,9 @@
+package lk.ijse.hello_shoes_shop_backend.Service;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public interface DateServices {
+    LocalDate orderLastReturnDay(Date orderPurchaseDate);
+
+}
